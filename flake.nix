9ac273b80e8291942233a15e12a942c8c29f2d3b
Constantin Gierczak--Galle <git.cst1@mailbox.org>
2025-09-25 14:52:01 +0200

    Feat: add flake

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "cst1.fr website";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }@attr:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.jekyll
+          ];
+        };
+      }
+    );
+}
